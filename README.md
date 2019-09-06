d5d43aea02e0a002be54ab44eb644c3b1a3d52ab
Erdem Arslan <erdemarslan@users.noreply.github.com>
2019-09-06 21:47:20 +0300

    requirement

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # piStatus
  Raspberry Pi Status Monitor with SSD1306 OLED Screen and Fan Control
+
+This project is under development.
+
+**HOW TO USE IT**
+Install required library for git
+
+    sudo apt-get install git
+
+Clone it from github.
+
+    cd ~
+    git clone https://github.com/erdemarslan/piStatus.git
+    cd piStatus
+
+Install required modules.
+
+    pip3 install -r requirement.txt
+
+Enable I2C from Raspberry Configuration and Reboot your RPI.
+
+Run the code
+
+    cd ~/piStatus
+    python3 pistatus.py
+
+If you want it start on Raspberry Pi Startup you must do that.
+Open terminal.
+
+    sudo nano /etc/local.rc
+
+Add this line before exit(0)
+
+    sudo python3 /home/pi/piStatus/pistatus.py &
+
+Save and reboot your RPI.
--- pistatus.py
+++ pistatus.py
@@ -62,7 +62,7 @@ def getIPAddress():
     global netmask
     global gateway
     global network
-    
+
     try:
         IP = netifaces.ifaddresses(network)[2][0]['addr']
         netmask = netifaces.ifaddresses(network)[2][0]['netmask']
@@ -88,16 +88,16 @@ def calculate_cpu():
     global disk_used
     global disk_free
     global disk_percent
-    
+
     mem_total = psutil.virtual_memory()[0] / (2**30) # GB
     mem_used = psutil.virtual_memory()[3] / (2**30) # GB
     mem_available = psutil.virtual_memory()[1] / (2**30) # GB
-    
+
     disk_used = psutil.disk_usage('/')[1] / (2**30) #GB
     disk_free = psutil.disk_usage('/')[2] / (2**30) #GB
     disk_total = psutil.disk_usage('/')[0] / (2**30) #GB
     disk_percent = psutil.disk_usage('/')[3] #GB
-    
+
     if number < 10:
         cpu_temp_total += cpu.temperature
         cpu_percent_total += psutil.cpu_percent()
@@ -108,10 +108,10 @@ def calculate_cpu():
         number = 0
         cpu_temp_total = 0
         cpu_percent_total = 0
-        
+
     t = Timer(0.1, calculate_cpu)
     t.start()
-    
+
 calculate_cpu()
 
 def get_date_time():
@@ -139,22 +139,22 @@ raise_screen_no()
 
 while True:
     draw.rectangle((0,0,width,height), outline=0, fill=0)
-    
-    
+
+
     if screen_no == 1:
         image = Image.open("/home/pi/piStatus/icon/temperature.bmp").convert("1")
         draw = ImageDraw.Draw(image)
-    
+
         var_temp = "{0:.2f} C".format(cpu_temp)
         var_cpu_load = "{0:.2f} %".format(cpu_percent)
         var_fan_speed = "Fan Speed: {0}%".format(fan_speed)
-    
+
         draw.text((x+20, top+4), "CPU Information   ",  font=font, fill=255)
         draw.text((x+55, top+20), var_temp, font=font, fill=255)
         draw.line((x+50, top+36, x+100, top+36), fill=255)
         draw.text((x+55, top+36), var_cpu_load, font=font, fill=255)
         draw.text((x+20, top+52), var_fan_speed, font=font, fill=255)
-        
+
     elif screen_no == 2:
         image = Image.open("/home/pi/piStatus/icon/network.bmp").convert("1")
         draw = ImageDraw.Draw(image)
@@ -163,46 +163,46 @@ while True:
         draw.text((x+55, top+20), str(IP), font=font, fill=255)
         draw.text((x+55, top+36), str(netmask), font=font, fill=255)
         draw.text((x+55, top+52), str(gateway), font=font, fill=255)
-        
+
     elif screen_no == 3:
         image = Image.open("/home/pi/piStatus/icon/memory.bmp").convert("1")
         draw = ImageDraw.Draw(image)
-        
+
         var_mem_total = "T: {0:.2f} GB".format(mem_total)
         var_mem_used = "U: {0:.2f} GB".format(mem_used)
         var_mem_available = "F: {0:.2f} GB".format(mem_available)
-        
+
         draw.text((x+20, top+4), "RAM Information  ",  font=font, fill=255)
         draw.text((x+55, top+20), var_mem_used, font=font, fill=255)
         draw.text((x+55, top+36), var_mem_available, font=font, fill=255)
         draw.text((x+55, top+52), var_mem_total, font=font, fill=255)
-        
+
     elif screen_no == 4:
         image = Image.open("/home/pi/piStatus/icon/disk.bmp").convert("1")
         draw = ImageDraw.Draw(image)
-        
+
         var_disk_total = "T: {0:.2f} GB".format(disk_total)
         var_disk_used = "U: {0:.2f} GB".format(disk_used)
         var_disk_free= "F: {0:.2f} GB".format(disk_free)
         var_disk_percent = "{0:.2f}%".format(disk_percent)
-        
-        
+
+
         draw.text((x+20, top+4), "Disk Information  ",  font=font, fill=255)
         draw.text((x+55, top+20), var_disk_used, font=font, fill=255)
         draw.text((x+55, top+36), var_disk_free, font=font, fill=255)
         draw.text((x+55, top+52), var_disk_total, font=font, fill=255)
         draw.text((x+10, top+52), var_disk_percent, font=font, fill=255)
-        
+
     else:
         image = Image.open("/home/pi/piStatus/icon/time.bmp").convert("1")
         draw = ImageDraw.Draw(image)
-        
+
         draw.text((x+35, top+4), "Date - Time  ",  font=font, fill=255)
         draw.text((x+55, top+20), date_text, font=font, fill=255)
         draw.text((x+55, top+36), time_text, font=font, fill=255)
         draw.text((x+55, top+52), weekday_text, font=font, fill=255)
-        
-        
+
+
     # Display image.
     disp.image(image)
     disp.display()
